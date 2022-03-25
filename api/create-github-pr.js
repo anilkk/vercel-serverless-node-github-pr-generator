44d41f29723b480ca05927bf8dad3904b35f4f19
Anil Kumar <anil.kumar@lokalise.com>
2022-03-25 12:42:14 +0100

    Update github code

--- api/create-github-pr.js
+++ api/create-github-pr.js
@@ -1,7 +1,23 @@
-export default function handler(request, response) {
+import { Octokit } from "@octokit/core";
+require('dotenv').config();
+
+export default async function handler(request, response) {
+    const octokit = new Octokit({ auth: process.env.GITHUB_API_TOKEN }),
+        owner = 'anilkk',
+        repo = 'test-repolisten-to-lokalise-webhook-repo',
+        title = 'My Lokalise test Pull Request',
+        body = 'This pull request is a test!',
+        head = 'my-lokalise-feature-branch',
+        base = 'main';
+
+
+    const response = await octokit.request(
+        `POST /repos/{owner}/{repo}/pulls`, { owner, repo, title, body, head, base }
+    );
+
     response.status(200).json({
-      body: request.body,
-      query: request.query,
-      cookies: request.cookies,
+        body: request.body,
+        query: request.query,
+        cookies: request.cookies,
     });
-  }
+}
